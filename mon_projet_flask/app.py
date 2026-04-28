b5b8ce1a9c5d026bd54fe5bb4ac884d894e5af0c
maryamsaghir1213-source <maryamsaghir1213@gmail.com>
2026-04-28 11:54:43 +0100

    Add games route to Flask application

--- mon_projet_flask/app.py
+++ mon_projet_flask/app.py
@@ -6,6 +6,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/games')
+def games():    
+    return render_template('games.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
